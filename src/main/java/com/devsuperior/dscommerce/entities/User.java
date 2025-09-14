7d8b52471b46d624b54119fbd947934dba85cdfd
Marlon Marques <marlonmarques6@gmail.com>
2025-09-14 20:51:18 -0300

    Order

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,87 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthData;
+    private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> Orders = new ArrayList<>();
+
+    public User(){}
+
+    public User(long id, String name, String email, String phone, LocalDate birthData, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthData = birthData;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthData() {
+        return birthData;
+    }
+
+    public void setBirthData(LocalDate birthData) {
+        this.birthData = birthData;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return Orders;
+    }
+}
