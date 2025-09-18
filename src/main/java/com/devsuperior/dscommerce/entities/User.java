85888fc3d62a6e77b5b1cf672016f15054c82cd2
Marlon Marques <marlonmarques6@gmail.com>
2025-09-18 16:00:09 -0300

    products amd category

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -14,6 +14,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String phone;
     private LocalDate birthData;
