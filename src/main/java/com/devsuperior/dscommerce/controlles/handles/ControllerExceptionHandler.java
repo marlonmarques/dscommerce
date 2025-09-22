26eb0a8efcff43dde37009c2122bc4cb2b5f328e
Marlon Marques <marlonmarques6@gmail.com>
2025-09-22 11:56:35 -0300

    Exception controller global

---
+++ src/main/java/com/devsuperior/dscommerce/controlles/handles/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscommerce.controlles.handles;
+
+import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.services.excepitos.ResourceNotFoundExcepiton;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundExcepiton.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundExcepiton e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
